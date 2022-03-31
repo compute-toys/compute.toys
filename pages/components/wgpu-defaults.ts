9aac5810912e91fda1b628dd119235572def9ef1
David A Roberts <d@vidr.cc>
2022-03-31 16:50:41 +1000

    extract common prelude

--- pages/components/wgpu-defaults.ts
+++ pages/components/wgpu-defaults.ts
@@ -3,31 +3,6 @@ export const default_entry_points = ["main_hist", "main_image"];
 export const default_shader = `
 // 2022 David A Roberts <https://davidar.io/>
 
-type int = i32;
-type uint = u32;
-type float = f32;
-
-type int2 = vec2<i32>;
-type int3 = vec3<i32>;
-type int4 = vec4<i32>;
-type uint2 = vec2<u32>;
-type uint3 = vec3<u32>;
-type uint4 = vec4<u32>;
-type float2 = vec2<f32>;
-type float3 = vec3<f32>;
-type float4 = vec4<f32>;
-
-struct Params { width: uint, height: uint, frame: uint };
-struct StorageBuffer { data: array<atomic<i32>> };
-
-@group(0) @binding(0) var<uniform> params: Params;
-@group(0) @binding(1) var col: texture_storage_2d<rgba16float,write>;
-@group(0) @binding(2) var<storage,read_write> buf: StorageBuffer;
-@group(0) @binding(3) var tex: texture_2d_array<f32>;
-@group(0) @binding(4) var texs: texture_storage_2d_array<rgba16float,write>;
-@group(0) @binding(5) var nearest: sampler;
-@group(0) @binding(6) var bilinear: sampler;
-
 // https://www.jcgt.org/published/0009/03/02/
 // https://www.pcg-random.org/
 fn pcg(seed: ptr<function, uint>) -> float {
