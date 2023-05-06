f1687d5b0912f2bfeeb3ef2ef9925c546c4b0378
David A Roberts <d@vidr.cc>
2023-05-06 19:30:04 +1000

    fix 0-indexed month

--- lib/util/dateutils.ts
+++ lib/util/dateutils.ts
@@ -1,7 +1,7 @@
 // helper to convert from supabase UTC date to standard euro-style year-month-day
 export const toDateString = (utcDate: string) => {
     const date = new Date(utcDate);
-    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 }
 
 export const toUnixTime = (utcDate: string) => {
