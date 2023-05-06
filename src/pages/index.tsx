912cdd9c4e22de0e0ca2f952c0f04d4da00dd6f1
David A Roberts <d@vidr.cc>
2023-05-06 15:42:27 +1000

    add standalone SPA editor

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { theme } from "theme/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ShadowCanvas } from "components/global/shadowcanvas";
+import FavIconHead from "components/global/faviconhead";
+import NoWgpuModal from "components/global/nowgpumodal";
+import { Editor } from 'components/editor/editor';
+import { useUpdateAtom } from 'jotai/utils';
+import { dbLoadedAtom } from 'lib/atoms/atoms';
+
+export default function App() {
+    // FIXME: pretend the DB has loaded otherwise wgputoycontroller silently fails for e.g. manual reloads
+    const setDBLoaded = useUpdateAtom(dbLoadedAtom);
+    setDBLoaded(true);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <FavIconHead />
+            <ShadowCanvas />
+            <CssBaseline />
+            <NoWgpuModal />
+            <Editor />
+        </ThemeProvider>
+    )
+}
