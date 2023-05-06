0a44e4dc82c426b10739dbe2f7d04844130a9119
David A Roberts <d@vidr.cc>
2023-05-06 19:26:59 +1000

    make canvas bigger on mobile

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -2,9 +2,9 @@
 // https://github.com/mui/material-ui/issues/15662
 
 import Monaco from 'components/editor/monaco';
-import {WgpuToyWrapper} from "components/wgputoy";
+import { WgpuToyWrapper } from "components/wgputoy";
 
-import {useCallback} from 'react';
+import { useCallback } from 'react';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -16,17 +16,17 @@ import ReloadButton from "components/buttons/reloadbutton";
 import ScaleButton from 'components/buttons/scalebutton';
 import FullscreenButton from "../buttons/fullscreenbutton";
 
-import {Frame} from "theme/theme";
+import { Frame } from "theme/theme";
 
 import "firacode";
 
 import UniformSliders from "components/editor/uniformsliders";
 import TexturePicker from "components/editor/texturepicker";
 import EntryPointDisplay from "components/editor/entrypointdisplay";
-import {canvasParentElAtom} from "lib/atoms/wgputoyatoms";
-import {useUpdateAtom} from "jotai/utils";
-import {MetadataEditor} from "components/editor/metadataeditor";
-import {saveColorTransitionSignalAtom} from "lib/atoms/atoms";
+import { canvasParentElAtom } from "lib/atoms/wgputoyatoms";
+import { useUpdateAtom } from "jotai/utils";
+import { MetadataEditor } from "components/editor/metadataeditor";
+import { saveColorTransitionSignalAtom } from "lib/atoms/atoms";
 import { ItemWithTransitionSignal } from 'theme/itemwithtransition';
 import Explainer from "./explainer";
 import ConfigurationPicker from "./configurationpicker";
@@ -42,8 +42,8 @@ export const Editor = () => {
         }
     }, []);
 
-    const Timer = dynamic(() => import('components/timer'), {ssr: false});
-    const Resolution = dynamic(() => import('components/resolution'), {ssr: false});
+    const Timer = dynamic(() => import('components/timer'), { ssr: false });
+    const Resolution = dynamic(() => import('components/resolution'), { ssr: false });
 
     let metadataEditor = null;
     if (supabase) {
@@ -54,61 +54,75 @@ export const Editor = () => {
         );
     }
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item ref={renderParentNodeRef} xs={3} md={4} lg={5} xl={6}>
-                    <ItemWithTransitionSignal transitionAtom={saveColorTransitionSignalAtom}>
-                        <Frame elevation={12}>
-                            <WgpuToyWrapper
-                                bindID={"editor-canvas"}
-                                style={{
-                                    display: 'inline-block',
-                                    borderRadius: '4px'
-                                }}
-                            />
-                        </Frame>
-                        <Grid container>
-                            <Grid item sx={{textAlign: 'left'}} xs={2}>
-                                <Timer />
-                            </Grid>
-                            <Grid item xs={7}>
-                                <PlayPauseButton />
-                                <ResetButton />
-                            </Grid>
-                            <Grid item sx={{textAlign: 'right'}} xs={3}>
-                                <Resolution />
-                                <ScaleButton />
-                                <FullscreenButton />
-                            </Grid>
-                        </Grid>
-                        <UniformSliders/>
-                    </ItemWithTransitionSignal>
-                    {metadataEditor}
-                </Grid>
-                <Grid item xs={9} md={8} lg={7} xl={6}>
-                    <ItemWithTransitionSignal transitionAtom={saveColorTransitionSignalAtom}>
-                        <Monaco
-                            editorOptions={{
-                                stopRenderingLineAfter: 1000,
-                                fontFamily: "'Fira Code', monospace",
-                                'bracketPairColorization.enabled': true,
-                                //fontLigatures: true,
-                            }}
-                        />
-                        <Box sx={{paddingTop: "4px"}}>
-                            <ReloadButton/>
-                            <HotReloadToggle/>
-                            <Explainer/>
-                        </Box>
-                    </ItemWithTransitionSignal>
-                    <Grid container spacing={2}>
-                        <Grid item><TexturePicker/></Grid>
-                        <Grid item><ConfigurationPicker/></Grid>
-                        <Grid item><EntryPointDisplay/></Grid>
+    const leftPanel = (
+        <div ref={renderParentNodeRef}>
+            <ItemWithTransitionSignal transitionAtom={saveColorTransitionSignalAtom}>
+                <Frame elevation={12}>
+                    <WgpuToyWrapper
+                        bindID={"editor-canvas"}
+                        style={{
+                            display: 'inline-block',
+                            borderRadius: '4px'
+                        }}
+                    />
+                </Frame>
+                <Grid container>
+                    <Grid item sx={{ textAlign: 'left' }} xs={2}>
+                        <Timer />
+                    </Grid>
+                    <Grid item xs={7}>
+                        <PlayPauseButton />
+                        <ResetButton />
+                    </Grid>
+                    <Grid item sx={{ textAlign: 'right' }} xs={3}>
+                        <Resolution />
+                        <ScaleButton />
+                        <FullscreenButton />
                     </Grid>
                 </Grid>
+                <UniformSliders />
+            </ItemWithTransitionSignal>
+            {metadataEditor}
+        </div>
+    );
+
+    const rightPanel = (
+        <div>
+            <ItemWithTransitionSignal transitionAtom={saveColorTransitionSignalAtom}>
+                <Monaco
+                    editorOptions={{
+                        stopRenderingLineAfter: 1000,
+                        fontFamily: "'Fira Code', monospace",
+                        'bracketPairColorization.enabled': true,
+                        //fontLigatures: true,
+                    }}
+                />
+                <Box sx={{ paddingTop: "4px" }}>
+                    <ReloadButton />
+                    <HotReloadToggle />
+                    <Explainer />
+                </Box>
+            </ItemWithTransitionSignal>
+            <Grid container spacing={2}>
+                <Grid item><TexturePicker /></Grid>
+                <Grid item><ConfigurationPicker /></Grid>
+                <Grid item><EntryPointDisplay /></Grid>
             </Grid>
-        </Box>
+        </div>
+    );
+
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={4} lg={5} xl={6}>
+                        {leftPanel}
+                    </Grid>
+                    <Grid item xs={12} md={8} lg={7} xl={6}>
+                        {rightPanel}
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
     );
 }
