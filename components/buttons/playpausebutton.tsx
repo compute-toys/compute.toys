f31985aa4c3f8e1b8105fb9f4e36c8ea1aa9c7be
wright <jeyko1@gmail.com>
2023-05-20 20:08:44 +0300

    remove pause-time-while-still-rendering function

--- components/buttons/playpausebutton.tsx
+++ components/buttons/playpausebutton.tsx
@@ -3,8 +3,7 @@ import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
 import { useAtom, useAtomValue } from 'jotai';
-import { pauseTimeWhileStillRenderingAtom, playAtom } from 'lib/atoms/atoms';
-import { useEffect, useState } from 'react';
+import { playAtom } from 'lib/atoms/atoms';
 
 const PlayPauseIcon = () => {
     const play = useAtomValue(playAtom);
@@ -13,47 +12,17 @@ const PlayPauseIcon = () => {
 
 export const PlayPauseButton = () => {
     const [play, setPlay] = useAtom(playAtom);
-    const [pauseTimeWhileStillRendering, setPauseTimeWhileStillRendering] = useAtom(
-        pauseTimeWhileStillRenderingAtom
-    );
-    const [ctrlKeyDown, setCtrlKeyDown] = useState(false);
     const theme = useTheme();
-
-    useEffect(() => {
-        const listenForCtrlKeyPress = e => {
-            if (e.code === 'ControlLeft' || e.metaKey) {
-                // will this work on mac?
-                setCtrlKeyDown(true);
-            }
-        };
-        const listenForCtrlKeyUnpress = e => {
-            if (e.code === 'ControlLeft' || e.metaKey) {
-                // will this work on mac?
-                setCtrlKeyDown(false);
-            }
-        };
-        window.addEventListener('keydown', listenForCtrlKeyPress);
-        window.addEventListener('keyup', listenForCtrlKeyUnpress);
-        return () => {
-            window.removeEventListener('keydown', listenForCtrlKeyPress);
-            window.removeEventListener('keyup', listenForCtrlKeyUnpress);
-        };
-    });
 
     return (
         <Button
             onClick={() => {
-                if (ctrlKeyDown && play === true) {
-                    setPauseTimeWhileStillRendering(true);
-                }
                 setPlay(!play);
             }}
-            title={'Play/Pause <Ctrl + Alt + Up> \nPause Time, but keep rendering <Ctrl + Click>'}
+            title={'Play/Pause <Ctrl + Alt + Up>\n'}
             sx={
                 play
                     ? { color: theme.palette.primary.contrastText }
-                    : pauseTimeWhileStillRendering
-                    ? { color: 'orange' }
                     : { color: theme.palette.primary.light }
             }
         >
