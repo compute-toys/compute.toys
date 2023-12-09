6ed9738121a10cc8c33bc2875031aec10be01593
David A Roberts <d@vidr.cc>
2023-12-09 08:52:25 +0000

    fix for FortAwesome/react-fontawesome#134

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Container from '@mui/material/Container';
