b850ef9592a369543e9cf6980fc516b8f427030d
alex <alexmarck12@gmail.com>
2024-08-23 04:04:23 -0600

    lint fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import FavIconHead from 'components/global/faviconhead';
 import LoginModal from 'components/global/loginmodal';
 import { ShadowCanvas } from 'components/global/shadowcanvas';
 import { AuthProvider } from 'lib/db/authcontext';
+import { WindowManagementProvider } from 'lib/util/draggablewindowscontext';
 import type { AppProps } from 'next/app';
 import { theme } from 'theme/theme';
-import { WindowManagementProvider } from '../lib/util/draggablewindowscontext';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
