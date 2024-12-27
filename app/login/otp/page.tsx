4b11c4fcb193ad8c430f71c1ed0cc169dbd2b1ae
David A Roberts <d@vidr.cc>
2024-12-27 19:10:22 +1000

    Use querystring

--- app/login/otp/page.tsx
+++ app/login/otp/page.tsx
@@ -2,11 +2,12 @@ import { verify } from '../actions';
 
 export const runtime = 'edge';
 
-export default function LoginPage() {
+export default async function LoginPage({ searchParams }) {
+    const email = (await searchParams).email;
     return (
         <form>
             <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" required />
+            <input id="email" name="email" type="email" required value={email} />
             <label htmlFor="token">Token:</label>
             <input id="token" name="token" type="text" required />
             <button formAction={verify}>Login / Sign up</button>
