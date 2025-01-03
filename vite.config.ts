8d871102db1144a3d63ae3bfb4e2aa709876c571
David A Roberts <d@vidr.cc>
2025-01-03 16:46:37 +1000

    Use relative base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react(), tsconfigPaths(), wasm(), topLevelAwait()]
 });
