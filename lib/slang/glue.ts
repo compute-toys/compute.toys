9c6ee7cbeae28a212d8903d139c7645c266ac997
David A Roberts <d@vidr.cc>
2025-05-05 10:58:04 +1000

    Lint

--- lib/slang/glue.ts
+++ lib/slang/glue.ts
@@ -36,10 +36,14 @@ class ShaderConverter {
         switch (type.kind) {
             case 'scalar':
                 switch (type.scalarType) {
-                    case 'float32': return 'f32';
-                    case 'uint32':  return 'atomic<u32>'; //why does this need to be atomic?
-                    case 'int32':   return 'i32';
-                    default:        return 'f32';
+                    case 'float32':
+                        return 'f32';
+                    case 'uint32':
+                        return 'atomic<u32>'; //why does this need to be atomic?
+                    case 'int32':
+                        return 'i32';
+                    default:
+                        return 'f32';
                 }
             case 'vector':
                 return `vec${type.elementCount}<${this.getWGSLType(type.elementType)}>`;
@@ -83,14 +87,19 @@ class ShaderConverter {
         return entryPoints
             .filter(ep =>
                 ep.userAttribs?.some(
-                    a => a.name === 'Cover' || a.name === 'WorkgroupCount' || a.name === 'DispatchCount'
+                    a =>
+                        a.name === 'Cover' ||
+                        a.name === 'WorkgroupCount' ||
+                        a.name === 'DispatchCount'
                 )
             )
             .map(ep => {
                 const lines: string[] = [];
                 const threadGroupSize = ep.threadGroupSize;
-                let countX = 0, countY = 0, countZ = 0;
-    
+                let countX = 0,
+                    countY = 0,
+                    countZ = 0;
+
                 const wgAttr = ep.userAttribs?.find(a => a.name === 'WorkgroupCount');
                 if (wgAttr && wgAttr.arguments.length >= 3) {
                     countX = wgAttr.arguments[0] as number;
@@ -105,25 +114,31 @@ class ShaderConverter {
                             if (param.type.baseShape === 'structuredBuffer') {
                                 countX = Math.ceil(this.getBufferSize(param) / threadGroupSize[0]);
                             } else if (param.type.baseShape === 'texture2D') {
-                                countX = countY = threadGroupSize[0] > 0 ? Math.ceil(512 / threadGroupSize[0]) : 1; //why 512?
+                                countX = countY =
+                                    threadGroupSize[0] > 0
+                                        ? Math.ceil(512 / threadGroupSize[0])
+                                        : 1; //why 512?
                             }
                         }
                     }
                 }
-                
+
                 // Set minimum counts and handle special case
                 countX = Math.max(countX, 1);
                 countY = Math.max(countY, 1);
                 countZ = Math.max(countZ, 1);
-    
+
                 lines.push(`#workgroup_count ${ep.name} ${countX} ${countY} ${countZ}`);
-    
+
                 const dispatchAttr = ep.userAttribs?.find(a => a.name === 'DispatchCount');
                 if (dispatchAttr) {
-                    const dispatchCount = dispatchAttr.arguments.length > 0 ? dispatchAttr.arguments[0] as number : 1;
+                    const dispatchCount =
+                        dispatchAttr.arguments.length > 0
+                            ? (dispatchAttr.arguments[0] as number)
+                            : 1;
                     lines.push(`#dispatch_count ${ep.name} ${dispatchCount}`);
                 }
-    
+
                 return lines.join('\n');
             })
             .join('\n');
