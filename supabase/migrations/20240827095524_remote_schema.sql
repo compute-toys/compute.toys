c771a4b6bc85bc2401040ca8e8e807e807e04e64
David A Roberts <d@vidr.cc>
2025-07-12 13:44:08 +1000

    Remove auth and storage functions from migrations
    https://github.com/orgs/supabase/discussions/34270

--- supabase/migrations/20240827095524_remote_schema.sql
+++ supabase/migrations/20240827095524_remote_schema.sql
@@ -1,59 +1,59 @@
-set check_function_bodies = off;
+-- set check_function_bodies = off;
 
-CREATE OR REPLACE FUNCTION storage.extension(name text)
- RETURNS text
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-_parts text[];
-_filename text;
-BEGIN
-	select string_to_array(name, '/') into _parts;
-	select _parts[array_length(_parts,1)] into _filename;
-	-- @todo return the last part instead of 2
-	return split_part(_filename, '.', 2);
-END
-$function$
-;
+-- CREATE OR REPLACE FUNCTION storage.extension(name text)
+--  RETURNS text
+--  LANGUAGE plpgsql
+-- AS $function$
+-- DECLARE
+-- _parts text[];
+-- _filename text;
+-- BEGIN
+-- 	select string_to_array(name, '/') into _parts;
+-- 	select _parts[array_length(_parts,1)] into _filename;
+-- 	-- @todo return the last part instead of 2
+-- 	return split_part(_filename, '.', 2);
+-- END
+-- $function$
+-- ;
 
-CREATE OR REPLACE FUNCTION storage.filename(name text)
- RETURNS text
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-_parts text[];
-BEGIN
-	select string_to_array(name, '/') into _parts;
-	return _parts[array_length(_parts,1)];
-END
-$function$
-;
+-- CREATE OR REPLACE FUNCTION storage.filename(name text)
+--  RETURNS text
+--  LANGUAGE plpgsql
+-- AS $function$
+-- DECLARE
+-- _parts text[];
+-- BEGIN
+-- 	select string_to_array(name, '/') into _parts;
+-- 	return _parts[array_length(_parts,1)];
+-- END
+-- $function$
+-- ;
 
-CREATE OR REPLACE FUNCTION storage.foldername(name text)
- RETURNS text[]
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-_parts text[];
-BEGIN
-	select string_to_array(name, '/') into _parts;
-	return _parts[1:array_length(_parts,1)-1];
-END
-$function$
-;
+-- CREATE OR REPLACE FUNCTION storage.foldername(name text)
+--  RETURNS text[]
+--  LANGUAGE plpgsql
+-- AS $function$
+-- DECLARE
+-- _parts text[];
+-- BEGIN
+-- 	select string_to_array(name, '/') into _parts;
+-- 	return _parts[1:array_length(_parts,1)-1];
+-- END
+-- $function$
+-- ;
 
-CREATE OR REPLACE FUNCTION storage.get_size_by_bucket()
- RETURNS TABLE(size bigint, bucket_id text)
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-    return query
-        select sum((metadata->>'size')::int) as size, obj.bucket_id
-        from "storage".objects as obj
-        group by obj.bucket_id;
-END
-$function$
-;
+-- CREATE OR REPLACE FUNCTION storage.get_size_by_bucket()
+--  RETURNS TABLE(size bigint, bucket_id text)
+--  LANGUAGE plpgsql
+-- AS $function$
+-- BEGIN
+--     return query
+--         select sum((metadata->>'size')::int) as size, obj.bucket_id
+--         from "storage".objects as obj
+--         group by obj.bucket_id;
+-- END
+-- $function$
+-- ;
 
 create policy "ALL avatars are publicly accessible."
 on "storage"."objects"
